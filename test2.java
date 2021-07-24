06a49b5ed7e4b1bf3f48cc25e2f2d1e674bb6dfa
“prem” <prem@ibm.com>
2021-07-24 06:45:17 +0000

    asjfgasdsdfsdg

---
+++ test2.java
@@ -0,0 +1,14 @@
+Mansukh Mandaviya to be Union minister for Health and Chemical and Fertilisers minster   sdmnfsdmbf minster 
+Jyotiraditya Scindia to monitor Civil Aviation ministry
+Railway, IT, Telecom ministries go to Ashwini Vaishnav
+Piyush Goyal to get Textile Ministry
+Hardeep Singh Puri to get charge of Petroleum Ministry
+Anurag Thakur gets Sports, Youth affairs and I&B
+Meenakshi Lekhi to be MoS External Affairs
+Dharmendra Pradhan to be new Education Minister
+Giriraj Singh to get Ministry of Rural Development
+Smriti Irani to be the Woman and Child Development Minister
+Bhupender Yadav to take charge of Labor Ministry
+Pashupati Paras gets Ministry of Food Processing Industries
+Kiren Rijiju to head Ministry of Law and Order
+Sarbananda Sonowal to monitor Ports, Shipping and waterways, and Ministry of AYUSH
